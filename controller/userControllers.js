0dee641f8eeb366e558fd37e29156f0dab95d0c8
MdialloC19 <mdiallo3211@gmail.com>
2024-02-01 23:55:09 +0000

    jwt token for autorization and auth ok

--- controller/userControllers.js
+++ controller/userControllers.js
@@ -1,17 +1,46 @@
 const User = require("../models/users");
-const { check, validationResult } = require("express-validator");
-const gravitar = require("gravitar");
-
+const { validationResult } = require("express-validator");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 /**
  * @desc get user from database by his email
  * @method
  * @return a user
  */
 
-// module.exports = async (req,res) => {
+exports.getAllUser = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    });
+  }
+  const { email } = req.body;
 
-//     const
-// };
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        erros: [{ msg: "User Not found" }],
+      });
+    }
+
+    return res.json(200).json({
+      sucess: true,
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      errors: [
+        {
+          msg: error.message,
+        },
+      ],
+    });
+  }
+};
 
 exports.postOneUser = async (req, res) => {
   const errors = validationResult(req);
@@ -20,17 +49,24 @@ exports.postOneUser = async (req, res) => {
       errors: errors.array(),
     });
   }
+  // console.log(req.body);
 
   const { name, email, password } = req.body;
 
   try {
     //verify if use exist
-    let user = await User.findOne(email);
+    let user = await User.findOne({ email });
     if (user) {
       return res.status(400).json({
         erros: [{ msg: "User already exists" }],
       });
     }
+
+    const avatar = gravatar.url(email, {
+      s: "200",
+      r: "pg",
+      d: "404",
+    });
     user = new User({
       name,
       email,
@@ -38,19 +74,41 @@ exports.postOneUser = async (req, res) => {
       password,
     });
 
-    user.avatar = gravitar.url(email, {
-      s: "200",
-      r: "pg",
-      d: "404",
-    });
+    const salt = await bcrypt.genSalt(10);
 
-    const salt = await bcrypt.genSaltSync(10);
-    user.password = await bcrypt.hashSync(password, salt);
+    user.password = await bcrypt.hash(password, salt);
+
     await user.save();
+
+    console.log(user.password);
+    await user.save();
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      process.env.mySecret,
+      {
+        expiresIn: Math.floor(Date.now() / 1000) + 60 * 60,
+      },
+      (err, token) => {
+        if (err) throw err;
+        return res.status(201).json({
+          token,
+          sucess: true,
+          message: "User registered",
+        });
+      }
+    );
   } catch (error) {
-    console.log(errors);
-    return res.status(500).json({
-      errors: [{ msg: errors.message() }],
+    console.log(error);
+    res.status(500).json({
+      sucess: false,
+      error: [error.message],
     });
   }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const userControllers = require("../controller/userControllers");
+const { check } = require("express-validator");
 
-router.post("/", userControllers.postOneUser);
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  userControllers.postOneUser
+);
 
 module.exports = router;
